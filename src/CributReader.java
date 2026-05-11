82394c1b010a5697baf6b9653e9378aade428191
SantiagooLozada <santiagoolozadab@gmail.com>
2026-05-11 14:07:27 +0200

    Proyecto acabado, todo funciona correctamente sin errores ninguno

--- src/CributReader.java
+++ src/CributReader.java
@@ -4,8 +4,17 @@ import java.io.IOException;
 
 public class CributReader {
 
+	//Para esta funcion, se esta leyendo un archivo .txt que es totalemente explicito, no es necesario crear un bucle porque no lees muchos registros diferentes
 	public static Cribut readCributFile (String fileName) throws IOException {
-		throw new UnsupportedOperationException ("TO-DO");
+		BufferedReader input = new BufferedReader(new FileReader(fileName));
+		String nom = input.readLine();
+		char element = input.readLine().charAt(0);
+		short salut = Short.parseShort(input.readLine());
+		String costAtac = input.readLine();
+		short danyAtac = Short.parseShort(input.readLine());
+		input.close();
+
+		return new Cribut(nom, element, salut, costAtac, danyAtac);
 	}
 
 }
